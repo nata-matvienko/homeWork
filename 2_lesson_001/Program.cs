a19555c0fbc26d7dfa7d186f46cf7184d9ac8d53
NATALI <nata_matvienk@mail.ru>
2022-02-07 21:28:19 +0200

    задачка про последнюю цифру 3х значного числа

--- 2_lesson_001/Program.cs
+++ 2_lesson_001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать последнюю цифру трехзначного числа
+int GetLastDigit(int number)
+{
+    return number % 10;
+}
+int GetIntegerFromConsole(int min, int max)
+{
+    int result = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.Write($"Введите целое число от {min} по {max}: ");
+        string input = Console.ReadLine();
+        result = int.Parse(input);
+        isError = result<min || result> max;
+    }
+    return result;
+}
+int number = GetIntegerFromConsole(100,999);
+int lastDigit = GetLastDigit(number);
+Console.WriteLine(lastDigit);
