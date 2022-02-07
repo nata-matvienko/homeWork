a8afce60755020de80063c8bf980c5ce88492fac
NATALI <nata_matvienk@mail.ru>
2022-02-07 22:12:40 +0200

    задачка про наибольшую цифру 2х значного числа

---
+++ 2_lesson_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(10, 100);
+    return randomNumber;
+}
+int number = GetRandomNumber();
+Console.WriteLine($"Сгенерированное число: {number}");
+
+int firstDigit = number/10;
+int lastDigit = number%10;
+
+if (firstDigit>lastDigit) 
+Console.WriteLine($"Наибольшее из цифр {firstDigit}");
+else 
+Console.WriteLine($"Наибольшее из цифр {lastDigit}");
