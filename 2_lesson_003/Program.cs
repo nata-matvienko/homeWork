cf333c1cb108223300d78a90728823ddf18eb41f
NATALI <nata_matvienk@mail.ru>
2022-02-07 22:53:55 +0200

    задачка про кратные числа и остаток

---
+++ 2_lesson_003/Program.cs
@@ -0,0 +1,33 @@
+﻿// Выяснить кратно ли число заданному, если нет, вывести остаток
+
+int GetNumber()
+{
+    int randomNumber = new Random().Next(20,100);
+    return randomNumber;
+}
+void isMultiple(int firstNumber, int secondNumber)
+{
+    if (firstNumber>secondNumber)
+    {
+        int result = firstNumber%secondNumber;
+        if (result == 0)
+            Console.WriteLine("Первое число кратно второму");
+        else
+        Console.WriteLine($"Остаток: {result}");
+    }
+    else
+    {
+        int result = secondNumber%firstNumber;
+        if (result == 0)
+            Console.WriteLine("Второе число кратно первому");
+        else
+        Console.WriteLine($"Остаток: {result}");
+    }
+}
+int number = GetNumber();
+Console.WriteLine($"Случайное число {number}");
+
+int secondNumber = GetNumber();
+Console.WriteLine($"Второе случайное число: {secondNumber}");
+isMultiple(number, secondNumber);
+
