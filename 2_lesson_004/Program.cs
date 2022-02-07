c51bff504f76a4019fc341d29c0b2c329dd480ad
NATALI <nata_matvienk@mail.ru>
2022-02-07 23:03:10 +0200

    задачка про деление на 7 и 23

---
+++ 2_lesson_004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int number = 152;
+if(CheckIfModed(number))
+    Console.WriteLine($"Число {number} делится на 7 и на 23");
+else
+    Console.WriteLine($"Число {number} не делится на 7 и на 23");
+
+bool CheckIfModed(int number)
+{
+    return number % 7 == 0 && number % 23 ==0;
+}
