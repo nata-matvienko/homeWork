2973dbc84253d731d9460c6be634dcaba5c28ef3
NATALI <nata_matvienk@mail.ru>
2022-02-10 00:30:35 +0200

    задача про дни недели

---
+++ 16_task/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число, обозначающее день недели. Выяснить является ли № дня выходным.
+
+int numberX = new Random().Next(1,8);
+Console.WriteLine($"Номер дня недели: {numberX}");
+int number = numberX / 10;
+if(numberX > 5)
+{
+    Console.WriteLine($"{numberX} день недели - выходной");
+}
+else
+{
+    Console.WriteLine($"{numberX} день недели - рабочий");
+}
+    
