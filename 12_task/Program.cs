36509fbcac663e9d4fdfeabac67deea3da0c1216
NATALI <nata_matvienk@mail.ru>
2022-02-09 23:45:22 +0200

    задача про удаление 2ой цифры

---
+++ 12_task/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить 2ю цифру 3х значного числа.
+
+int numberABC = new Random().Next(100,999);
+Console.WriteLine($"Случайное число: {numberABC}");
+int numberA = numberABC / 100;
+int numberC = numberABC % 10;
+
+Console.WriteLine($"{numberA}{numberC}");
