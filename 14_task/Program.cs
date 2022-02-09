34d5a3229c4d87122ab485fe9027fb3a8dd21388
NATALI <nata_matvienk@mail.ru>
2022-02-10 00:11:53 +0200

    задача про наличие третьей цифры

---
+++ 14_task/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Найти 3ю цифру числа или сообщить что ее нет.
+
+ int numberX = new Random().Next(0,1000);
+Console.WriteLine($"Случайное число: {numberX}");
+int number = numberX / 10;
+if(number >= 10)
+{
+    Console.WriteLine($"Третья цифра = {numberX%10}");
+}
+else
+{
+    Console.WriteLine($"Третья цифра отсутствует");
+}
+
