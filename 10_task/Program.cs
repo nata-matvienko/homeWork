a64a4fc54ba075a28b6cc5e9a40b2efd3d596308
NATALI <nata_matvienk@mail.ru>
2022-02-09 23:17:35 +0200

    задача про 2ю цифру в 3х значном числе

---
+++ 10_task/Program.cs
@@ -0,0 +1,6 @@
+﻿// 10. Показать 2ю цифру 3х значного числа.
+
+int numberABC = new Random().Next(100,999);
+Console.WriteLine($"Случайное число: {numberABC}");
+int numberB = numberABC%100/10;
+Console.WriteLine($"Вторая цифра = {numberB}");
