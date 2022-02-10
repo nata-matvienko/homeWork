ffe0ed6cc46ded121bd8eeb6ea73539335ab10b4
NATALI <nata_matvienk@mail.ru>
2022-02-10 16:16:31 +0200

    задача про ПАЛИНДРОМ

---
+++ 3_leson_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+/* int InPut(string msg)
+{
+    Console.Write(msg);
+    string num1 = console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+int numOne = InPut("Введите пятизначное число: ");
+if () */
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if(int.TryParse(number, out int valueInt))
+{
+    string resultValue = valueInt.ToString();
+    if(resultValue[0]==resultValue[4] && resultValue[1]==resultValue[3])
+    Console.WriteLine("ПАЛИНДРОМ");
+    else
+    Console.WriteLine("НЕ ПАЛИНДРОМ");
+}
