fc361658cb16a4fa38c579479d03c52628e79c2c
NATALI <nata_matvienk@mail.ru>
2022-02-10 14:49:34 +0200

    задача про квадрат чисел, является ли одно квадратом другого

---
+++ 3_lesson_17/Program.cs
@@ -0,0 +1,23 @@
+﻿// 17. По двум заданным числам проверить является ли одно квадратом другого.
+int InPut(string msg)
+{
+    Console.WriteLine(msg);
+    string num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+int numOne = InPut("Введите первое число: ");
+int numTwo = InPut("Введите второе число: ");
+if(numOne==numTwo*numTwo)
+{
+    Console.WriteLine($"{numOne} является квадратом числа {numTwo}");
+}
+else 
+if (numTwo==numOne*numOne)
+{
+    Console.WriteLine($"{numTwo} является квадратом числа {numOne}");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
