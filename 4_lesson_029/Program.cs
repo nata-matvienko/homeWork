04139282395e9a47d45263de1bb1cd98777e82f7
NATALI <nata_matvienk@mail.ru>
2022-02-16 22:24:09 +0200

    задача про произведение чисел до n

---
+++ 4_lesson_029/Program.cs
@@ -0,0 +1,22 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+int multiple(int number)
+{
+    int result = 1;
+    for(int i = 1; i<number; i++)
+    {
+        result = i * result;
+    }
+    return result;
+}
+int number = InPut("Введите число N: ");
+int resultofmultiple = multiple(number);
+
+Console.WriteLine(resultofmultiple);
