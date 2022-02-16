9badcf1d5e519a63cd60b0c5d5ee7291419067e0
NATALI <nata_matvienk@mail.ru>
2022-02-16 21:50:12 +0200

    задача про СУММУ ЧИСЕЛ ДО а

---
+++ 3_lesson_025/Program.cs
@@ -0,0 +1,16 @@
+﻿// 25. Найти сумму чисел от 1 до А.
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+int number = InPut("Введите число, до которого хотите найти сумму: ");
+int sum = 0;
+for (int i = 1; i<=number; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Сумма всех чисел до требуемой = {sum}");
