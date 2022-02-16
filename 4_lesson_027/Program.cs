a71a491db1192502c6d21e8ef26d6f05cada16c6
NATALI <nata_matvienk@mail.ru>
2022-02-16 22:04:28 +0200

    задача про колличество цифр в числе

---
+++ 4_lesson_027/Program.cs
@@ -0,0 +1,20 @@
+﻿// 27. определить количество цифр в числе
+
+// 123/10 = 12/10 = 1/10 = 0
+//            1       2     3
+
+int GetNumber() // получаем число
+{
+    int randomNumber = new Random().Next(100, 1000000);
+    return randomNumber;
+}
+int number = GetNumber();
+Console.WriteLine(number);
+
+int countofDigit = 0;
+while (number != 0)
+{
+    number = number/10;
+    countofDigit++;
+}
+Console.WriteLine(countofDigit);
