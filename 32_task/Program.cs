62c6b86a0bb7f14b7c2b2bb557edf005cbf93795
NATALI <nata_matvienk@mail.ru>
2022-02-20 22:49:21 +0200

    32 задача про массив из 8 эементов

---
+++ 32_task/Program.cs
@@ -0,0 +1,26 @@
+﻿// // 32. Задать массив из 8 элементов, заполненных нулями и единицами и вывести на экран
+
+
+int[] array = new int [8];
+void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index=0;
+    while(index<length)
+    {
+        collection[index]=new Random().Next(0,2);
+        index++;
+    }
+}
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position=0;
+    while(position<count)
+    {
+        Console.Write($"{col[position]}");
+        position++;
+    }
+}
+FillArray(array);
+PrintArray(array);
