1b5d548c536040f65dbdd5242d3c209407de2803
NATALI <nata_matvienk@mail.ru>
2022-02-20 21:19:38 +0200

    26 задача про а в степени в

---
+++ 26_task/Program.cs
@@ -0,0 +1,29 @@
+﻿// 26. Возведите число А в натуральную степень В, используя цикл
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int n = int.Parse(Console.ReadLine());    
+double exp = Exponentiation(num, n);
+double Exponentiation( int num, int n)
+{
+    double exp = 1;
+    for (int i=0; i<n; i++)
+    {
+        exp = exp*num;
+    }
+    return exp;
+}
+Console.WriteLine(exp);
+   
+
+/* РАБОЧИЙ НО БЕЗ ЦИКЛА!!!
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = int.Parse(Console.ReadLine());
+//int res =num;
+double result = Math.Pow(a, b);
+Console.WriteLine($"{a}^{b}={result}");   */
+
