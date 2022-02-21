3dc1dacbf7e02b6f33151639ed2ebd3fc1b40f04
NATALI <nata_matvienk@mail.ru>
2022-02-21 07:09:48 +0200

    28 задача про сумму чисел

---
+++ 28_task/Program.cs
@@ -0,0 +1,10 @@
+﻿// 28. Посчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (num>0)
+{
+    sum = sum+num%10;
+    num = num/10;
+}
+Console.WriteLine($"Сумма цифр = {sum}");
