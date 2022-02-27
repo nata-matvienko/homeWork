0eb23be058bf777c3fc0fb5a2612dc07fe1ad0bc
NATALI <nata_matvienk@mail.ru>
2022-02-27 22:22:47 +0200

    34 задача про замену элементов массива на противоположные

---
+++ 34_task/Program.cs
@@ -0,0 +1,12 @@
+﻿// 34. Написать программу замены элементов массива на противоположные.
+
+int [] array = {3, 1, 5, -7, 13};
+void result (int[]array)
+{
+    for (int index=0; index < array.Length; index++)
+    {
+        array[index] = array[index] * -1;
+        Console.Write($"{array[index]}, ");
+    }
+}
+result(array);             
