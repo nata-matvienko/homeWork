02b868a87796b1986f0498324eb2a7ee78541763
NATALI <nata_matvienk@mail.ru>
2022-02-27 23:38:11 +0200

    36 задача про четные и нечетные 3х значные чмсла в массиве

---
+++ 36_task/Program.cs
@@ -0,0 +1,19 @@
+﻿// 36. Задать массив, заполнить его случайными положительными 3-х значными числами. 
+// Показать количество нечетных/четных чисел.
+
+int even = 0;
+int odd = 0;
+int [] array = new int[9];
+
+Console.WriteLine("Задан массив: ");
+for (int index = 0; index<array.Length; index++)
+{
+    array [index] = new Random().Next(100, 1000);
+    Console.Write($"{array[index]}, ");
+    if (array [index]%2==0)
+        even = even + 1;
+    else
+        odd = odd + 1;
+}
+Console.WriteLine($"Четных чисел: {even}");
+Console.WriteLine($"Нечетных чисел: {odd}");
