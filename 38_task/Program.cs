0482e84d4009ff4d14664511bef10a21b5035267
NATALI <nata_matvienk@mail.ru>
2022-02-28 00:02:33 +0200

    38 задача про сумму чисел массива на нечетной позиции

---
+++ 38_task/Program.cs
@@ -0,0 +1,14 @@
+﻿// 38. Найти сумму чисел 1 мерного массива, стоящих на нечетной позиции.
+
+int sum = 0;
+int [] array = new int[9];
+Console.WriteLine("Задан массив: ");
+for (int index = 0; index<array.Length; index++)
+{
+    array [index] = new Random().Next(1, 100);
+    Console.Write($"{array[index]}, ");
+    if (index%2==0)
+        sum = sum + array[index];
+}
+Console.WriteLine($"Сумма чисел массива из нечетных позиций = {sum}");
+
