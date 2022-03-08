d7448c5098533165924bceae6f3311f30b261c65
NATALI <nata_matvienk@mail.ru>
2022-03-09 01:13:53 +0200

    количество положительных чисел, введенных с клавиатуры

---
+++ 42_task/Program.cs
@@ -0,0 +1,13 @@
+﻿// 42. Определить, сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите числа: ");
+string[] n = Console.ReadLine().Split(' ');
+int[] array = new int[n.Length];
+int count = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    array[i] = Convert.ToInt32(n[i]);
+    if (array[i] > 0)
+    count++;   
+}
+Console.WriteLine($"Введено {count} чисел > 0");
