38ff33b76389df3a0c62e5bc4c89f7aa65e57cfe
NATALI <nata_matvienk@mail.ru>
2022-03-13 16:15:44 +0200

    про 2мерный массив и противоположные четные эл-ты

---
+++ 50_task/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 50: В двумерном массиве n×k заменить четные элементы на противоположные.
+
+int [,] matrix = new int [3,4];
+for (int n=0; n<3; n++)
+{
+    for (int k=0; k<4; k++)
+    {
+        matrix[n,k]=new Random().Next(1,10);
+        if (matrix[n, k] % 2 == 0)
+            {
+                matrix[n, k] = matrix[n, k] * -1;
+            }
+        Console.Write($"{matrix[n,k]} ");
+    }
+    Console.WriteLine();
+}
+
