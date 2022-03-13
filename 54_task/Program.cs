1ffb20f7cf279a77c839881c15ebe6a330967f1c
NATALI <nata_matvienk@mail.ru>
2022-03-13 23:23:49 +0200

    про сумму эл-тов главной диагонали

---
+++ 54_task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 54: В матрице чисел найти сумму элементов главной диагонали.
+
+int [,] matrix = new int [5,5];
+int result = 0;
+for (int n=0; n<5; n++)
+{
+    for (int k=0; k<5; k++)
+    {
+        matrix[n,k]=new Random().Next(1,10);
+        if(n == k) result += matrix[n, k]; 
+        Console.Write($"{matrix[n,k]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine($"{result}");
